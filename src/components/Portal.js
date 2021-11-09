1bfb785a71230f7bbc88c622d01fc7c59dd9316b
khairun nafi <khairunnafi000@gmail.com>
2021-11-09 17:30:35 +0800

    studentportal + cssupdate

--- src/components/Portal.js
+++ src/components/Portal.js
@@ -1,34 +1,43 @@
 const Portal = ({ Logout, Subject, onToggle, onAdd, listSubject }) => {
   const onAddSubj = (e) => {
-    let prevlistSubject=[];
+    let prevlistSubject = [];
 
     /*{Subject.map((Subject)=>Subject.selected ? onAdd(Subject.subject):'')}*/
-    {Subject.map((Subject)=>Subject.selected&&!Subject.registered ? listSubject.push(Subject.subject):'')}
+    {
+      Subject.map((Subject) =>
+        Subject.selected && !Subject.registered
+          ? listSubject.push(Subject.subject)
+          : ""
+      );
+    }
 
-    onAdd(listSubject)
+    onAdd(listSubject);
   };
 
   const onDelSubj = (e) => {
-    
-
     /*{Subject.map((Subject)=>Subject.selected ? onAdd(Subject.subject):'')}*/
-    {Subject.map((Subject)=>!Subject.selected&&Subject.registered ? listSubject.push(Subject.subject):'')}
+    {
+      Subject.map((Subject) =>
+        !Subject.selected && Subject.registered
+          ? listSubject.push(Subject.subject)
+          : ""
+      );
+    }
 
-    onAdd(listSubject)
+    onAdd(listSubject);
   };
 
   return (
     <div>
-      
       <div className="topheader">
-      <header className="toptext">Student Portal</header>
-      <button className="topright" onClick={Logout}>Logout</button>
+        <header className="toptext">Student Portal</header>
+        <button className="topright" onClick={Logout}>
+          Logout
+        </button>
       </div>
-     
 
       <div className="leftmenu">
-        <h1 className="subjectregister">Subjects offered</h1>
-
+        <h1 className="subjectregister">Subjects offered </h1>
         {Subject.map(
           (Subject) =>
             !Subject.registered && (
@@ -42,7 +51,7 @@ const Portal = ({ Logout, Subject, onToggle, onAdd, listSubject }) => {
         )}
 
         <div className="bottomright">
-          <button className="btn btn-block" onClick={onAddSubj} >
+          <button className="btn btn-block" onClick={onAddSubj}>
             Register
           </button>
         </div>
@@ -50,32 +59,32 @@ const Portal = ({ Logout, Subject, onToggle, onAdd, listSubject }) => {
 
       <div className="rightmenu">
         <h1 className="subjectregister">Registered subject</h1>
-        
-        <div className='tableleft'>
-        {Subject.map(
-          (Subject) =>
-            Subject.registered && (
-              <div
-                className={`task ${Subject.selected ? "reminder" : ""}`}
-                onClick={() => onToggle(Subject.id)}
-              >
-                <h3 key={Subject.id}>{Subject.subject}</h3>
-              </div>
-            )
-        )}
+
+        <div className="tableleft">
+          {Subject.map(
+            (Subject) =>
+              Subject.registered && (
+                <div
+                  className={`task ${Subject.selected ? "reminder" : ""}`}
+                  onClick={() => onToggle(Subject.id)}
+                >
+                  <h3 key={Subject.id}>{Subject.subject}</h3>
+                </div>
+              )
+          )}
         </div>
 
-        <div className='tableright'>
-        {Subject.map(
-          (Subject) =>
-            Subject.registered && (
-              <div className={`taskcolorless`}>
-                <h3 key={Subject.id}>{Subject.marks}</h3>
+        <div className="tableright">
+          {Subject.map(
+            (Subject) =>
+              Subject.registered && (
+                <div className={`taskcolorless`}>
+                  <h3 key={Subject.id}>{Subject.marks}</h3>
                 </div>
-            )
-        )}
+              )
+          )}
         </div>
-        
+
         <div className="bottomright">
           <button className="btn btn-block" onClick={onDelSubj}>
             Delete
