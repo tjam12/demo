418f0f95c8b66df77ec42ce63c78f73545bdf3a5
hepyf99 <hepy0608@gmail.com>
2021-11-09 22:45:02 +0800

    hepy

--- src/components/Lecturer.js
+++ src/components/Lecturer.js
@@ -1,30 +1,86 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Lecturer = ({ Logout, Students, Subject, onToggle, onAdd, listSubject }) => {
-    return (
-        <div>
-            <header>Lecturer Portal</header>
-      <button style='display:inline-block' onClick={Logout}>Logout</button>
-            {Students.map((Students) =>
+const Lecturer = ({ Logout,
+  Students,
+  Subject,
+  onToggle,
+  onAdd,
+  listSubject,
+}) => {
+  let subjects = ["Mathematics", "Java", "HCI"];
+  const [displaySbj, setSbj] = useState();
+  const [, setMark] = useState();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setMark(Students);
+  };
+
+  return (
+    <div>
+      <div className="topheader">
+        <h1 className="headertext">Lecturer Portal</h1>
+        <button className="btn" onClick={Logout}>Logout</button>
+      </div>
+
+        <div className="leftmenu">
+          <h2 className="subjectborder">Subjects</h2>
+          <div className="subjectlistborder">
+          {Students.map((students) => (
             
-              <div
-              >
-                <h3 key={Students.id}>{Students.name}</h3>
-              </div>
-            
-        )}
-        
-{Students.map((Students) =>Students.name === 'Ali' && (
-            <div>
-        {Students.subjects.map(subjects => 
-          <h3>{subjects}</h3>
-        )}
-            </div>)
-      )}
-      <p>HEADER</p>
+            <h4>{students.names}</h4>
+
+          ))}
+          </div>
         </div>
-    )
-}
+  
 
-export default Lecturer
+      <div className="rightmenu">
+        <h2 className="studentnameborder">Student Name</h2>
+        <div className="studnamelistborder">
+        {displaySbj}
+        
+        
+        {subjects.map((result) => (
+          <p>
+            <label>
+              <>
+                <input
+                  type="radio"
+                  value={result}
+                  name="radiovalues"
+                  onChange={(e) => setSbj(e.target.value)}
+                />
+              </>
+              {result}
+            </label>
+          </p>
+        ))}
+        <keyinmark onSubmit={submitHandler}>
+        <div className="keyintable">
+            {Students.map((students) => (
+              <p>
+                <label>
+                  <>
+                    <div> 
+                     <input type="text" onChange={<h4>{students.marks}</h4>} />
+                     </div> 
+                  </>
+                </label>
+              </p>
+            ))}
+         </div>
+        </keyinmark>
+        </div>
+      </div>
 
+      <div className="bottommenuleft">
+        <button className="btn" onClick={Logout}>
+          Submit
+        </button>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Lecturer;
